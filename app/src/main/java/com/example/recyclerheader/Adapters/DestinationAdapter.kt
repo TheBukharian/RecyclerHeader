b152ff5307d6ee80aa4bba9490c85fdc9797f12c
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-11-30 16:57:13 +0500

    Done Task

--- app/src/main/java/com/example/recyclerheader/Adapters/DestinationAdapter.kt
+++ app/src/main/java/com/example/recyclerheader/Adapters/DestinationAdapter.kt
@@ -1,20 +1,126 @@
 package com.example.recyclerheader.Adapters
 
-import android.content.Context
-import android.widget.Button
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerheader.R
-
-import com.xwray.groupie.GroupieViewHolder
-import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.item_add_destination.view.*
 import kotlinx.android.synthetic.main.red_item.view.*
 
-class DestinationAdapter(): Item<GroupieViewHolder>() {
 
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+class DestinationAdapter(var dataList : ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    inner class footerHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        init {
+            Log.d("lastindex", "${dataList.lastIndex}")
+
+
+
+            itemView.addBtn.setOnClickListener {
+                if (dataList.size < 4){
+                    itemView.addBtn.visibility = View.VISIBLE
+                    dataList.add(0 , "owioeiwoe")
+                    notifyItemInserted(0)
+                }else{
+                    itemView.addBtn.visibility = View.GONE
+                    dataList.add(0 , "owioeiwoe")
+                    notifyItemInserted(0)
+                }
+
+
+            }
+        }
+
+
+        fun bindFooter(){
+
+        }
+
+
 
 
     }
-    override fun getLayout(): Int = R.layout.item_add_destination
+    inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        init {
+            itemView.delBtn.setOnClickListener {
 
+                if (dataList.size < 4){
+                    dataList.removeAt(adapterPosition)
+                    notifyItemRemoved(adapterPosition)
+                    notifyItemChanged(dataList.lastIndex)
+
+                }else{
+                    dataList.removeAt(adapterPosition)
+                    notifyItemRemoved(adapterPosition)
+                    notifyItemChanged(dataList.lastIndex)
+                }
+
+            }
+        }
+    }
+
+    private val TYPE_ITEM : Int = 1
+    private val TYPE_FOOTER : Int = 2
+
+    private var onItemClick : View.OnClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType == TYPE_ITEM) {
+            val view  = LayoutInflater.from(parent.context).inflate(R.layout.item_add_destination, parent, false)
+            view.setOnClickListener(onItemClick)
+            return Holder(view)
+        }else  {
+            val footerView = LayoutInflater.from(parent.context).inflate(R.layout.red_item, parent, false)
+            return footerHolder(footerView)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(isPositionFooter(position)) {
+            TYPE_FOOTER
+        }else {
+            TYPE_ITEM
+        }
+    }
+
+
+
+
+    override fun getItemCount(): Int {
+       return  dataList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+
+        if(isPositionFooter(position)){
+            val footerHolder=holder as footerHolder
+            if(dataList.lastIndex != 4){
+
+                footerHolder.itemView.addBtn.visibility = View.VISIBLE
+            } else{
+                footerHolder.itemView.addBtn.visibility = View.GONE
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+    private fun isPositionFooter(position: Int): Boolean{
+        return position == dataList.lastIndex
+    }
 
 }
+
+
+
+
--- app/src/main/java/com/example/recyclerheader/MainActivity.kt
+++ app/src/main/java/com/example/recyclerheader/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recyclerheader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerheader.Adapters.*
 import com.xwray.groupie.GroupAdapter
@@ -12,13 +13,16 @@ import kotlinx.android.synthetic.main.red_item.*
 class MainActivity : AppCompatActivity() {
 
     private val adapter = GroupAdapter<GroupieViewHolder>()
-    val redAdapter = GroupAdapter<GroupieViewHolder>()
+
+//    val redAdapter = GroupAdapter<GroupieViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val mainRecycler = findViewById<RecyclerView>(R.id.mainRecycler)
+
+
 
 
 
@@ -37,24 +41,15 @@ class MainActivity : AppCompatActivity() {
         adapter.add(ItemView(ins = RoadInfo("OOOOOOOPPOPOP", "kjekrjekrj")))
         recyclerView.adapter = adapter
 
-        redAdapter.add(RedAdapter())
+
+
+       val  data = arrayListOf<String>()
+
+        data.add("wewewewew")
+
+
+        val redAdapter = DestinationAdapter(data)
         mainRecycler.adapter = redAdapter
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
