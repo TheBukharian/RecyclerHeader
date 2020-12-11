725e810367c016ac2b0674bcc65574854ed8811d
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-12-11 16:01:33 +0500

    Done Task

--- app/src/main/java/com/example/recyclerheader/Adapters/DestinationAdapter.kt
+++ app/src/main/java/com/example/recyclerheader/Adapters/DestinationAdapter.kt
@@ -2,18 +2,28 @@ package com.example.recyclerheader.Adapters
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerheader.MainActivity
 import com.example.recyclerheader.R
 import kotlinx.android.synthetic.main.item_add_destination.view.*
 import kotlinx.android.synthetic.main.red_item.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
-class DestinationAdapter(var dataList : ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class DestinationAdapter(val activity: MainActivity, var dataList: ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
-    inner class footerHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    var isDeleteBoolean=false
+
+
+    inner class footerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         init {
             Log.d("lastindex", "${dataList.lastIndex}")
@@ -21,102 +31,135 @@ class DestinationAdapter(var dataList : ArrayList<String>) : RecyclerView.Adapte
 
 
             itemView.addBtn.setOnClickListener {
-                if (dataList.size < 4){
+                if (dataList.size < 4) {
                     itemView.addBtn.visibility = View.VISIBLE
-                    dataList.add(0 , "owioeiwoe")
-                    notifyItemInserted(0)
-                }else{
+                } else {
                     itemView.addBtn.visibility = View.GONE
-                    dataList.add(0 , "owioeiwoe")
-                    notifyItemInserted(0)
                 }
+
+                dataList.add(0, "owioeiwoe")
+                notifyItemInserted(dataList.lastIndex)
+
+
+
 
 
             }
         }
 
 
-        fun bindFooter(){
-
-        }
-
-
-
-
     }
-    inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val move = itemView.findViewById<ImageButton>(R.id.moveBtn)
+
         init {
+            
             itemView.delBtn.setOnClickListener {
 
-                if (dataList.size < 4){
+                if (dataList.size < 4) {
                     dataList.removeAt(adapterPosition)
                     notifyItemRemoved(adapterPosition)
-                    notifyItemChanged(dataList.lastIndex)
 
-                }else{
+                } else {
                     dataList.removeAt(adapterPosition)
                     notifyItemRemoved(adapterPosition)
-                    notifyItemChanged(dataList.lastIndex)
                 }
-
+                CoroutineScope(Dispatchers.Main).launch{
+                    coroutine(1000)
+                }
             }
         }
+
+        private suspend fun coroutine(millis: Long) {
+            delay(millis)
+            notifyDataSetChanged()
+        }
+
+
+
     }
 
-    private val TYPE_ITEM : Int = 1
-    private val TYPE_FOOTER : Int = 2
+    private val TYPE_ITEM: Int = 1
+    private val TYPE_FOOTER: Int = 2
 
-    private var onItemClick : View.OnClickListener? = null
+    private var onItemClick: View.OnClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if(viewType == TYPE_ITEM) {
-            val view  = LayoutInflater.from(parent.context).inflate(R.layout.item_add_destination, parent, false)
+        if (viewType == TYPE_ITEM) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_add_destination, parent, false)
             view.setOnClickListener(onItemClick)
+
+
             return Holder(view)
-        }else  {
+        } else {
             val footerView = LayoutInflater.from(parent.context).inflate(R.layout.red_item, parent, false)
             return footerHolder(footerView)
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if(isPositionFooter(position)) {
+        return if (isPositionFooter(position)) {
             TYPE_FOOTER
-        }else {
+        } else {
             TYPE_ITEM
         }
     }
 
 
-
-
     override fun getItemCount(): Int {
-       return  dataList.size
+        return dataList.size
     }
+
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
 
-        if(isPositionFooter(position)){
-            val footerHolder=holder as footerHolder
-            if(dataList.lastIndex != 4){
+        if (getItemViewType(position) == TYPE_ITEM) {
+
+
+            if(isDeleteBoolean){
+                holder.itemView.moveBtn.visibility = View.GONE
+                holder.itemView.delBtn.visibility = View.VISIBLE
+            } else{
+                holder.itemView.moveBtn.visibility = View.VISIBLE
+                holder.itemView.delBtn.visibility = View.GONE
+            }
+
+
+            holder.itemView.moveBtn.setOnTouchListener { v, event ->
+                if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                    activity.touchHelper?.startDrag(holder)
+                }
+
+                false
+            }
+            holder.itemView.black.text = (position + 1).toString()
+        }
+
+
+
+        if (isPositionFooter(position)) {
+            val footerHolder = holder as footerHolder
+            if (dataList.lastIndex != 4) {
 
                 footerHolder.itemView.addBtn.visibility = View.VISIBLE
-            } else{
+            } else {
                 footerHolder.itemView.addBtn.visibility = View.GONE
             }
 
         }
+    }
 
+    private fun isPositionFooter(position: Int): Boolean {
+        return position == dataList.lastIndex
     }
 
 
+    fun changeItemButton() {
+       isDeleteBoolean=!isDeleteBoolean
 
-
-
-
-    private fun isPositionFooter(position: Int): Boolean{
-        return position == dataList.lastIndex
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/example/recyclerheader/MainActivity.kt
+++ app/src/main/java/com/example/recyclerheader/MainActivity.kt
@@ -2,28 +2,33 @@ package com.example.recyclerheader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerheader.Adapters.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
-import kotlinx.android.synthetic.main.red_item.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private val adapter = GroupAdapter<GroupieViewHolder>()
+    var touchHelper: ItemTouchHelper? =null
+    val  data = arrayListOf<String>()
+    val redAdapter = DestinationAdapter(this,data)
 
-//    val redAdapter = GroupAdapter<GroupieViewHolder>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val mainRecycler = findViewById<RecyclerView>(R.id.mainRecycler)
-
-
-
 
 
         adapter.add(HeaderView())
@@ -43,14 +48,61 @@ class MainActivity : AppCompatActivity() {
 
 
 
-       val  data = arrayListOf<String>()
-
         data.add("wewewewew")
 
-
-        val redAdapter = DestinationAdapter(data)
         mainRecycler.adapter = redAdapter
 
+                 touchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0){
+                    override fun onMove(
+                        recyclerView: RecyclerView,
+                        p1: RecyclerView.ViewHolder,
+                        p2: RecyclerView.ViewHolder
+                    ): Boolean  {
+                        val fromPos = p1.adapterPosition
+                        val toPos = p2.adapterPosition
+                        Collections.swap(data,fromPos,toPos)
+                        redAdapter.notifyItemMoved(fromPos,toPos)
+
+                        CoroutineScope(Dispatchers.Main).launch{
+                            coroutine(1000)
+                        }
+
+
+
+                        return true
+                    }
+
+                    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) { }
+                })
+                touchHelper!!.attachToRecyclerView(mainRecycler)
+
     }
+
+    suspend fun coroutine( millis:Long) {
+        delay(millis)
+
+
+        redAdapter.notifyDataSetChanged()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_items,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.selector){
+                redAdapter.changeItemButton()
+
+
+            Log.i("elrjgerg","ekrgrg")
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
 
